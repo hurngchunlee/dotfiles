e0c1713461f7d21cd236272566456a2a818529ee
Hurng-Chun Lee <h.lee@donders.ru.nl>
2019-09-19 13:49:00 +0200

    Added ranger_devicons plugin

--- install-ranger.sh
+++ install-ranger.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 sudo pip install ranger-fm
-
 sudo apt install rxvt w3m w3m-img
+
+echo 'install ranger devicon plugin ...'
+git clone https://github.com/alexanderjeurissen/ranger_devicons ~/tmp/ranger_devicons && \
+cd ~/tmp/ranger_devicons && make install
