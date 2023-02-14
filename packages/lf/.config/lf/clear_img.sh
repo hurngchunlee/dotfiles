164177c0dbd8e08ccd87ab8336eb0727833dd528
Hurng-Chun Lee <h.lee@donders.ru.nl>
2023-02-14 16:17:27 +0100

    workaround for kitty preview issue
     - https://github.com/gokcehan/lf/issues/1099

--- packages/lf/.config/lf/clear_img.sh
+++ packages/lf/.config/lf/clear_img.sh
@@ -2,5 +2,6 @@
 [ -n "$FIFO_UEBERZUG" ] &&
     printf '{"action": "remove", "identifier": "preview"}\n' >"$FIFO_UEBERZUG"
 
+## Notice the kitty's breaking change: https://github.com/gokcehan/lf/issues/1099
 [ -n "$(which kitty 2>/dev/null)" ] &&
-    kitty +icat --clear --silent --transfer-mode file
+    kitty +kitten icat --clear --stdin no --silent --transfer-mode file < /dev/null > /dev/tty
--- packages/lf/.config/lf/draw_kitty.sh
+++ packages/lf/.config/lf/draw_kitty.sh
@@ -1,9 +1,10 @@
-#!/bin/bash
+#!/bin/bash -x
 f=$1
 w=$2
 h=$3
 x=$4
 y=$5
 
+## Notice the kitty's breaking change: https://github.com/gokcehan/lf/issues/1099
 [ -n "$(which kitty 2>/dev/null)" ] &&
-    kitty +icat --silent --transfer-mode file --place "${w}x${h}@${x}x${y}" "$f"
+    kitty +kitten icat --silent --transfer-mode file --stdin no --place "${w}x${h}@${x}x${y}" "$f" < /dev/null > /dev/tty
